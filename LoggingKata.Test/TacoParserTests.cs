f8ec1144fcd021b2c3fec436cae308563930c87f
logan-tolbert <logo88205+git@gmail.com>
2024-08-22 13:29:53 -0500

    Update code

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -24,21 +24,26 @@ namespace LoggingKata.Test
         //Add additional inline data. Refer to your CSV file.
         public void ShouldParseLongitude(string line, double expected)
         {
-            // TODO: Complete the test with Arrange, Act, Assert steps below.
-            //       Note: "line" string represents input data we will Parse 
-            //       to extract the Longitude.  
-            //       Each "line" from your .csv file
-            //       represents a TacoBell location
-
+          
             //Arrange
-
+            var tacoParser = new TacoParser();
             //Act
-
+            var actual = tacoParser.Parse(line);
             //Assert
+            Assert.Equal(expected, actual.Location.Longitude);
         }
 
-
-        //TODO: Create a test called ShouldParseLatitude
-
+        [Theory]
+        [InlineData("34.073638, -84.677017, Taco Bell Acwort...", 34.073638)]
+        
+        public void ShouldParseLatitude(string line, double expected)
+        {
+            //Arrange
+            var tacoParser = new TacoParser();
+            //Act
+            var actual = tacoParser.Parse(line);
+            //Assert
+            Assert.Equal(expected, actual.Location.Latitude);
+        }
     }
 }
--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -1,4 +1,7 @@
-﻿namespace LoggingKata
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LoggingKata
 {
     /// <summary>
     /// Parses a POI file to locate all the Taco Bells
@@ -6,7 +9,7 @@
     public class TacoParser
     {
         readonly ILog logger = new TacoLogger();
-        
+
         public ITrackable Parse(string line)
         {
             logger.LogInfo("Begin parsing");
@@ -17,36 +20,39 @@
             // If your array's Length is less than 3, something went wrong
             if (cells.Length < 3)
             {
-                // Log error message and return null
-                return null; 
+                logger.LogError("Something went wrong....");// Log error message and return null
+                return null;
             }
 
             // TODO: Grab the latitude from your array at index 0
             // You're going to need to parse your string as a `double`
             // which is similar to parsing a string as an `int`
-            
-            
+            var isLatitude = double.TryParse(cells[0], out double latitude);
+
             // TODO: Grab the longitude from your array at index 1
             // You're going to need to parse your string as a `double`
             // which is similar to parsing a string as an `int`
-            
-            
-            // TODO: Grab the name from your array at index 2
-            
+            var isLongitude = double.TryParse(cells[1], out double longitude);
 
-            // TODO: Create a TacoBell class
-            // that conforms to ITrackable
-            
+            // TODO: Grab the name from your array at index 2
+            string name = cells[2];
+
             // TODO: Create an instance of the Point Struct
             // TODO: Set the values of the point correctly (Latitude and Longitude) 
+            var coordinates = new Point();
+            coordinates.Latitude = latitude;
+            coordinates.Longitude = longitude;
 
             // TODO: Create an instance of the TacoBell class
             // TODO: Set the values of the class correctly (Name and Location)
+            TacoBell tacoBell = new();
+            tacoBell.Name = name;
+            tacoBell.Location = coordinates;
 
             // TODO: Then, return the instance of your TacoBell class,
             // since it conforms to ITrackable
+            return tacoBell;
 
-            return null;
         }
     }
 }
